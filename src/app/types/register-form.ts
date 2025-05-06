610d6ef07b681f3e164439223d7f8795906a870b
Joe Kim <joesm.genie@gmail.com>
2025-05-06 17:02:17 +0700

    add type register form

---
+++ src/app/types/register-form.ts
@@ -0,0 +1,12 @@
+type RegisterForm = {
+  username: string;
+  password: string;
+  confirmPassword: string;
+  sex: string;
+  email: string;
+  verifyCode: string;
+}
+
+export type {
+  RegisterForm,
+}
