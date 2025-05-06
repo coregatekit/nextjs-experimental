0852b04c33d5d8bcd557a92953cfc07415425943
Joe Kim <joesm.genie@gmail.com>
2025-05-06 17:24:13 +0700

    create username input

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,7 +5,9 @@ import type { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { registerFormSchema } from '../form-schema/register-form'
-import { Form } from '@/components/ui/form'
+import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import RegisterScreenLabel from './label'
+import { Input } from '@/components/ui/input'
 
 export default function Register() {
   const form = useForm<z.infer<typeof registerFormSchema>>({
@@ -20,7 +22,7 @@ export default function Register() {
     },
   })
 
-  const { handleSubmit } = form
+  const { control, handleSubmit } = form
 
   const onSubmit = (data: z.infer<typeof registerFormSchema>) => {
     console.log('data', data)
@@ -28,12 +30,20 @@ export default function Register() {
 
   return (
     <div className='mt-16 flex flex-col items-center justify-center'>
-      <div>Registration</div>
+      <div>{RegisterScreenLabel.title}</div>
+      <div>{RegisterScreenLabel.subtitle}</div>
 
       {/* Register form */}
       <Form {...form}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          
+        <form onSubmit={handleSubmit(onSubmit)} className='mt-12'>
+          {/* Username */}
+          <FormField control={control} name='username' render={({ field }) => (
+            <FormItem>
+              <FormLabel>{RegisterScreenLabel.username}</FormLabel>
+              <Input placeholder={RegisterScreenLabel.placeholder.username} {...field}/>
+              <FormMessage />
+            </FormItem>
+          )} />
         </form>
       </Form>
     </div>
