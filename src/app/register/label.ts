7c5bc09fbb940d392b955c076998a5df894ac7c3
Joe Kim <joesm.genie@gmail.com>
2025-05-06 17:42:26 +0700

    implement button check existing user with mock username

--- src/app/register/label.ts
+++ src/app/register/label.ts
@@ -5,13 +5,19 @@ const RegisterScreenLabel = {
   email: 'Email Address',
   password: 'Password',
   confirmPassword: 'Confirm Password',
-  registerButton: 'Register',
   placeholder: {
     username: 'Enter your username',
     email: 'Enter your email address',
     password: 'Enter your password',
     confirmPassword: 'Confirm your password again',
     verifyCode: 'Enter the verification code that shown on the screen',
+  },
+  button: {
+    checkUser: 'Check Username',
+    register: 'Register',
+  },
+  message: {
+    existUser: 'User already taken',
   }
 }
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,9 +5,16 @@ import type { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { registerFormSchema } from '../form-schema/register-form'
-import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
 import RegisterScreenLabel from './label'
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 export default function Register() {
   const form = useForm<z.infer<typeof registerFormSchema>>({
@@ -22,10 +29,19 @@ export default function Register() {
     },
   })
 
-  const { control, handleSubmit } = form
+  const { control, handleSubmit, watch, setError } = form
 
   const onSubmit = (data: z.infer<typeof registerFormSchema>) => {
     console.log('data', data)
+  }
+
+  const handleClickCheckUser = () => {
+    if (watch('username') === 'aerichandesu') {
+      setError('username', {
+        type: 'manual',
+        message: RegisterScreenLabel.message.existUser,
+      })
+    }
   }
 
   return (
@@ -35,15 +51,29 @@ export default function Register() {
 
       {/* Register form */}
       <Form {...form}>
-        <form onSubmit={handleSubmit(onSubmit)} className='mt-12'>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className='mt-12 flex w-96 flex-col gap-4'
+        >
           {/* Username */}
-          <FormField control={control} name='username' render={({ field }) => (
-            <FormItem>
-              <FormLabel>{RegisterScreenLabel.username}</FormLabel>
-              <Input placeholder={RegisterScreenLabel.placeholder.username} {...field}/>
-              <FormMessage />
-            </FormItem>
-          )} />
+          <FormField
+            control={control}
+            name='username'
+            render={({ field }) => (
+              <FormItem className='flex flex-col gap-2'>
+                <FormLabel>{RegisterScreenLabel.username}</FormLabel>
+                <div className='flex gap-2'>
+                  <Input
+                    placeholder={RegisterScreenLabel.placeholder.username}
+                    {...field}
+                    className='w-2/3'
+                  />
+                  <Button type="button" onClick={handleClickCheckUser}>{RegisterScreenLabel.button.checkUser}</Button>
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </form>
       </Form>
     </div>
