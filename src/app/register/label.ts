e228755f47857af7da564f1d226aa34bb84689e1
Joe Kim <joesm.genie@gmail.com>
2025-05-06 17:23:59 +0700

    create register screen label

---
+++ src/app/register/label.ts
@@ -0,0 +1,18 @@
+const RegisterScreenLabel = {
+  title: 'Create an account',
+  subtitle: 'Please fill in the details below to create your account.',
+  username: 'Username',
+  email: 'Email Address',
+  password: 'Password',
+  confirmPassword: 'Confirm Password',
+  registerButton: 'Register',
+  placeholder: {
+    username: 'Enter your username',
+    email: 'Enter your email address',
+    password: 'Enter your password',
+    confirmPassword: 'Confirm your password again',
+    verifyCode: 'Enter the verification code that shown on the screen',
+  }
+}
+
+export default RegisterScreenLabel
