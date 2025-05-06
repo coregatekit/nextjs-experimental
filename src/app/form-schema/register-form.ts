d0e85ab5e64d282307c7f0e3075c4a7b2be0de8e
Joe Kim <joesm.genie@gmail.com>
2025-05-06 17:06:08 +0700

    create register form schema

---
+++ src/app/form-schema/register-form.ts
@@ -0,0 +1,24 @@
+import { z, type ZodType } from 'zod'
+import type { RegisterForm } from '../types/register-form'
+
+const registerFormSchema: ZodType<RegisterForm> = z.object({
+  username: z
+    .string()
+    .min(6, { message: 'Username must be at least 6 characters long' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long' }),
+  confirmPassword: z
+    .string()
+    .min(6, { message: 'Confirm password must be at least 6 characters long' }),
+  sex: z.enum(['Male', 'Female'], { message: 'Please select' }),
+  email: z
+    .string()
+    .email({ message: 'Invalid email address' })
+    .min(1, { message: 'Email is required' }),
+  verifyCode: z.string().min(6, {
+    message: 'Verification code must be at least 6 characters long',
+  }),
+})
+
+export { registerFormSchema }
