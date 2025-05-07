ef03d7664bfa13659428769e38288be5db1baed9
Joe Kim <joesm.genie@gmail.com>
2025-05-07 10:47:21 +0700

    add required message for register form

--- src/app/form-schema/register-form.ts
+++ src/app/form-schema/register-form.ts
@@ -4,23 +4,25 @@ import { EnumSex } from '../enums/sex'
 
 const registerFormSchema: ZodType<RegisterForm> = z.object({
   username: z
-    .string()
+    .string({ required_error: 'Username is required' })
     .min(6, { message: 'Username must be at least 6 characters long' }),
   password: z
-    .string()
+    .string({ required_error: 'Password is required' })
     .min(6, { message: 'Password must be at least 6 characters long' }),
   confirmPassword: z
-    .string()
+    .string({ required_error: 'Confirm password is required' })
     .min(6, { message: 'Confirm password must be at least 6 characters long' }),
   sex: z.enum(
     [EnumSex.Male, EnumSex.Female, EnumSex.Other, EnumSex.PreferNotToSay],
-    { message: 'Please select' },
+    { required_error: 'Sex is required' },
   ),
   email: z
-    .string()
+    .string({ required_error: 'Email is required' })
     .email({ message: 'Invalid email address' })
     .min(1, { message: 'Email is required' }),
-  verifyCode: z.string().min(6, {
+  verifyCode: z.string({
+    required_error: 'Verification code is required',
+  }).min(6, {
     message: 'Verification code must be at least 6 characters long',
   }),
 })
