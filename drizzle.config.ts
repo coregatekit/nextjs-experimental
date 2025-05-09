7fecbb8a8d3736d4dd100cae6a72952771035622
Joe Kim <joesm.genie@gmail.com>
2025-05-09 17:08:50 +0700

    change path schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   dialect: 'sqlite',
-  schema: './src/db/schema/*',
+  schema: './src/db/schema.ts',
   dbCredentials: {
     url: process.env.DB_FILE_NAME || 'file:./mmo.db',
   }
