cb8a6c7e7400588ff06e22dfc3581d61fcf6c93a
Joe Kim <joesm.genie@gmail.com>
2025-05-16 14:43:04 +0700

    Change logo app

--- src/components/nav/nav-bar.tsx
+++ src/components/nav/nav-bar.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function NavBar() {
   return (
     <nav className='relative'>
-      <div className='bg-secondary dark:bg-primary-foreground fixed flex h-16 w-full flex-row items-center justify-between'>
-        <div>Logo</div>
+      <div className='bg-secondary dark:bg-primary-foreground fixed flex h-16 w-full flex-row items-center justify-between p-8'>
+        <Link href='/'>
+          <div className='text-2xl font-bold'>Heroes MaSter</div>
+        </Link>
         <div>Settings</div>
       </div>
     </nav>
