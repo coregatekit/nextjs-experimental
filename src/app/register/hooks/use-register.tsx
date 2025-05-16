c589fbcadd87f935827e7294754a71bd0dacf925
Joe Kim <joesm.genie@gmail.com>
2025-05-16 11:40:05 +0700

    add hook register

---
+++ src/app/register/hooks/use-register.tsx
@@ -0,0 +1,38 @@
+import { useMutation } from '@tanstack/react-query'
+import { RegisterRequest } from '@/app/types/register-form'
+
+export default function useRegister() {
+  const api = '/api/auth/signup'
+
+  const signupRequest = async (data: RegisterRequest) => {
+    try {
+      const response = await fetch(api, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+
+      if (!response.ok) {
+        const errorMessage = await response.json()
+        throw new Error(
+          errorMessage.message || 'An error occurred while signing up',
+        )
+      }
+
+      return response.json()
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(error.message)
+      }
+      throw new Error('An unknown error occurred')
+    }
+  }
+
+  const signup = useMutation({
+    mutationFn: signupRequest,
+    mutationKey: ['signup'],
+  })
+
+  return { signup }
+}
