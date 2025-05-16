b99a85383822efcb1960f481da4530c737bea682
Joe Kim <joesm.genie@gmail.com>
2025-05-16 16:33:52 +0700

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <div>Hello world</div>
+    <div className='flex flex-col items-center justify-center min-h-screen p-24'>
+      <h1 className='text-5xl font-bold'>Welcome to Heroes MaSter!</h1>
     </div>
   )
 }
