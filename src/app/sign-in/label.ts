6c05486e382a25cc2f37e1efb8242d1b4baa99dc
Joe Kim <joesm.genie@gmail.com>
2025-05-17 18:18:08 +0700

    create title and subtitle for sign in form

---
+++ src/app/sign-in/label.ts
@@ -0,0 +1,16 @@
+export const SignInScreenLabel = {
+  title: 'Sign In',
+  subtitle: 'Please enter your credentials to sign in',
+  username: {
+    label: 'Username',
+    placeholder: 'Enter your username',
+  },
+  password: {
+    label: 'Password',
+    placeholder: 'Enter your password',
+  },
+  submit: {
+    label: 'Sign In',
+    loadingLabel: 'Signing In...',
+  },
+}
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { SignInScreenLabel } from './label'
 
 export default function SignIn() {
-  return <div>SignIn</div>
+  return (
+    <div className='my-16 flex flex-col items-center justify-center'>
+      <div className='text-xl font-bold'>{SignInScreenLabel.title}</div>
+      <div className='text-lg'>{SignInScreenLabel.subtitle}</div>
+    </div>
+  )
 }
