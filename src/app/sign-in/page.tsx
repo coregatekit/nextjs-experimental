5896a04aee1a735021910a3c1d86a3aa8c7ade88
Joe Kim <joesm.genie@gmail.com>
2025-05-17 18:23:07 +0700

    create input username

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,9 +1,12 @@
+'use client'
+
 import React from 'react'
 import { SignInScreenLabel } from './label'
 import { useForm } from 'react-hook-form'
 import { signInFormSchema, SingInFormSchema } from '../definitions/sign-in'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Form } from '@/components/ui/form'
+import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 
 export default function SignIn() {
   const form = useForm<SingInFormSchema>({
@@ -23,8 +26,25 @@ export default function SignIn() {
 
       {/* Sign In form */}
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='mt-12 flex w-96 flex-col gap-4'
+        >
+          {/* Username */}
+          <FormField
+            control={form.control}
+            name='username'
+            render={({ field }) => (
+              <FormItem className='flex flex-col gap-2'>
+                <FormLabel>{SignInScreenLabel.username.label}</FormLabel>
+                <Input
+                  placeholder={SignInScreenLabel.username.placeholder}
+                  {...field}
+                />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </form>
       </Form>
     </div>
