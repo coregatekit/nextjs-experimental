a7a2eacce0caaf368738ea99bcffd93e64a1c238
Joe Kim <joesm.genie@gmail.com>
2025-05-20 20:52:50 +0700

    implement action sign in

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,22 +1,24 @@
 'use client'
 
-import React from 'react'
+import React, { useActionState } from 'react'
 import { SignInScreenLabel } from './label'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { useSearchParams } from 'next/navigation'
 import { LogIn } from 'lucide-react'
+import { authenticate } from '../actions/sign-in'
 
 export default function SignIn() {
   const searchParams = useSearchParams()
   const callbackUrl = searchParams.get('callbackUrl') || '/application'
+  const [message, action, pending] = useActionState(authenticate, undefined)
 
   return (
     <div className='my-16 flex flex-col items-center justify-center'>
       <div className='text-xl font-bold'>{SignInScreenLabel.title}</div>
       <div className='text-lg'>{SignInScreenLabel.subtitle}</div>
 
-      <form className='mt-12 flex w-96 flex-col gap-4'>
+      <form action={action} className='mt-12 flex w-96 flex-col gap-4'>
         <div className='flex flex-col gap-2'>
           <label htmlFor='username'>{SignInScreenLabel.username.label}</label>
           <Input
@@ -24,6 +26,7 @@ export default function SignIn() {
             name='username'
             type='text'
             placeholder={SignInScreenLabel.username.placeholder}
+            disabled={pending}
           />
         </div>
 
@@ -34,12 +37,19 @@ export default function SignIn() {
             name='password'
             type='password'
             placeholder={SignInScreenLabel.password.placeholder}
+            disabled={pending}
           />
         </div>
 
+        {message && <div className='text-red-500'>{message}</div>}
+
         <Input type='hidden' name='redirectTo' value={callbackUrl} />
 
-        <Button type='submit' className='w-full cursor-pointer'>
+        <Button
+          type='submit'
+          className='w-full cursor-pointer'
+          disabled={pending}
+        >
           <LogIn />
           {SignInScreenLabel.submit.label}
         </Button>
