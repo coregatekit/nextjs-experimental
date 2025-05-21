a0b6ab325a9721be31bce83e7f8384eb4885282a
Joe Kim <joesm.genie@gmail.com>
2025-05-21 12:23:46 +0700

    add cursor pointer

--- src/components/nav/nav-bar.tsx
+++ src/components/nav/nav-bar.tsx
@@ -98,7 +98,7 @@ export default function NavBar() {
               {status === 'authenticated' && (
                 <NavigationMenuItem>
                   <NavigationMenuLink
-                    className={cn(navigationMenuTriggerStyle())}
+                    className={cn(navigationMenuTriggerStyle(), 'cursor-pointer')}
                     onClick={handleSignOut}
                   >
                     Sign Out
