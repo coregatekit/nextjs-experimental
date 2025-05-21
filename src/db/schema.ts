98d6e5036f64eb704514316bc5e2d4e4a9904e48
Joe Kim <joesm.genie@gmail.com>
2025-05-21 13:02:52 +0700

    add table jobs

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
 import { sql } from 'drizzle-orm'
 import { createId } from '@paralleldrive/cuid2'
-import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 import { EnumSex } from '@/app/enums/sex'
 
@@ -12,6 +12,27 @@ export const usersTable = sqliteTable('users', {
   password: text('password').notNull(),
   sex: text('sex').notNull().default(EnumSex.PreferNotToSay),
   email: text('email').notNull().unique(),
+  createdAt: text('created_at')
+    .notNull()
+    .default(sql`(current_timestamp)`),
+  updatedAt: text('created_at')
+    .notNull()
+    .default(sql`(current_timestamp)`)
+    .$onUpdate(() => sql`(current_timestamp)`),
+})
+
+export const jobsTable = sqliteTable('jobs', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  name: text('name').notNull(),
+  weapon: text('weapon').notNull(),
+  description: text('description').notNull(),
+  hp: integer('hp').notNull(),
+  mp: integer('mp').notNull(),
+  attack: integer('attack').notNull(),
+  defense: integer('defense').notNull(),
+  evasion: integer('evasion').notNull(),
   createdAt: text('created_at')
     .notNull()
     .default(sql`(current_timestamp)`),
