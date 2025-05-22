fe8c16c99eec0a326c9b6cf53b9bf710823d931f
Joe Kim <joesm.genie@gmail.com>
2025-05-22 23:32:47 +0700

    cleanup migration scripts

--- drizzle/0002_gray_madrox.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `jobs` ADD `magic` integer NOT NULL;--> statement-breakpoint
-ALTER TABLE `jobs` ADD `magic_defense` integer NOT NULL;
--- drizzle/0004_amusing_mentallo.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `honor_classes` ADD `updated_at` text DEFAULT (current_timestamp) NOT NULL;--> statement-breakpoint
-ALTER TABLE `jobs` ADD `updated_at` text DEFAULT (current_timestamp) NOT NULL;--> statement-breakpoint
-ALTER TABLE `users` ADD `updated_at` text DEFAULT (current_timestamp) NOT NULL;
