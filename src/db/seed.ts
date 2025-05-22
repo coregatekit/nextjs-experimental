2314597342966e25a0493c8af8b383324b52e7cb
Joe Kim <joesm.genie@gmail.com>
2025-05-22 23:57:30 +0700

    add script seed classes table

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,10 +1,13 @@
-import { jobsTable } from './schema'
+import { classesTable, jobsTable } from './schema'
 import * as jobsData from './data/jobs.json'
+import * as classesData from './data/classes.json'
 import db from '.'
+import { eq } from 'drizzle-orm'
 
 function main() {
   console.log('Seeding database...')
   seedJobs()
+  seedClasses()
   console.log('Seeding completed!')
 }
 
@@ -32,6 +35,46 @@ async function seedJobs() {
   console.log('Seeding jobs...')
   await db.insert(jobsTable).values(data).onConflictDoNothing()
   console.log('Jobs seeded successfully!')
+}
+
+async function seedClasses() {
+  const data: (typeof classesTable.$inferInsert)[] = []
+
+  console.log('Preparing seed classes data...')
+  for (let i = 0; i < classesData.length; i++) {
+    const jobs = await db
+      .select({ id: jobsTable.id })
+      .from(jobsTable)
+      .where(eq(jobsTable.name, classesData[i].job))
+      .limit(1)
+      .execute()
+    if (jobs.length === 0) {
+      console.error(
+        `Job ${classesData[i].job} not found for class ${classesData[i].name}. Skipping...`,
+      )
+      continue
+    }
+    data.push({
+      name: classesData[i].name,
+      description: classesData[i].description,
+      classLevel: classesData[i].classLevel,
+      requiredHonor: classesData[i].requiredHonor,
+      hp: classesData[i].hp,
+      mp: classesData[i].mp,
+      attack: classesData[i].attack,
+      defense: classesData[i].defense,
+      magic: classesData[i].magicAttack,
+      magicDefense: classesData[i].magicDefense,
+      evasion: classesData[i].evasion,
+      jobId: jobs[0].id,
+    })
+  }
+
+  console.log('Clearing classes table...')
+  await db.delete(classesTable).execute()
+  console.log('Seeding classes...')
+  await db.insert(classesTable).values(data).onConflictDoNothing()
+  console.log('Classes seeded successfully!')
 }
 
 main()
